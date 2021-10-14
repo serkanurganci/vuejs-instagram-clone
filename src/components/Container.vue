899b309527272d6aef961877e59f2b392e49ca87
serkanurganci <serkan.urganci10@gmail.com>
2021-10-14 21:29:53 +0300

    Completed header component design

---
+++ src/components/Container.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Container'
+}
+</script>
+<style>
+.container {
+  max-width: 955px;
+  margin: 0 auto;
+}
+</style>
--- src/views/explore/index.vue
+++ src/views/explore/index.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'Home',
+  name: 'Explore',
   components: {}
 }
 </script>
