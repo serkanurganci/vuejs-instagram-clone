5cecaafd7113cc1636d2cc937b5fe10a9f3a1342
serkanurganci <serkan.urganci10@gmail.com>
2021-10-17 17:17:48 +0300

    Created Avatar Component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,36 @@
+<template>
+  <figure class="avatar">
+    <img :src="src" :alt="alt" :width="size" :height="size" />
+  </figure>
+</template>
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default:
+        'https://st.depositphotos.com/1052233/2885/v/600/depositphotos_28850541-stock-illustration-male-default-profile-picture.jpg'
+    },
+    alt: {
+      type: String,
+      default: 'Profile Picture'
+    },
+    size: {
+      type: String,
+      default: '56'
+    }
+  }
+}
+</script>
+<style>
+.avatar {
+  padding: 3px;
+  border-radius: 100%;
+  display: inline-flex;
+  background-image: linear-gradient(120deg, #fccb90 0%, #d57eeb 100%);
+}
+.avatar img {
+  border: 2px solid white;
+  border-radius: inherit;
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,95 +1,20 @@
 <template>
   <div class="home">
-    <div class="timeline">
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est magni
-        minima suscipit perferendis nam soluta, veniam voluptatibus! Eaque ipsum
-        ea, quam adipisci, assumenda dolor quod omnis quibusdam accusamus cumque
-        culpa modi voluptatem sunt ducimus consequuntur aliquam. Rem, commodi
-        inventore! Veniam sapiente accusamus nisi facilis expedita vel
-        provident. Vel, ullam aut!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est magni
-        minima suscipit perferendis nam soluta, veniam voluptatibus! Eaque ipsum
-        ea, quam adipisci, assumenda dolor quod omnis quibusdam accusamus cumque
-        culpa modi voluptatem sunt ducimus consequuntur aliquam. Rem, commodi
-        inventore! Veniam sapiente accusamus nisi facilis expedita vel
-        provident. Vel, ullam aut!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est magni
-        minima suscipit perferendis nam soluta, veniam voluptatibus! Eaque ipsum
-        ea, quam adipisci, assumenda dolor quod omnis quibusdam accusamus cumque
-        culpa modi voluptatem sunt ducimus consequuntur aliquam. Rem, commodi
-        inventore! Veniam sapiente accusamus nisi facilis expedita vel
-        provident. Vel, ullam aut!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est magni
-        minima suscipit perferendis nam soluta, veniam voluptatibus! Eaque ipsum
-        ea, quam adipisci, assumenda dolor quod omnis quibusdam accusamus cumque
-        culpa modi voluptatem sunt ducimus consequuntur aliquam. Rem, commodi
-        inventore! Veniam sapiente accusamus nisi facilis expedita vel
-        provident. Vel, ullam aut!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est magni
-        minima suscipit perferendis nam soluta, veniam voluptatibus! Eaque ipsum
-        ea, quam adipisci, assumenda dolor quod omnis quibusdam accusamus cumque
-        culpa modi voluptatem sunt ducimus consequuntur aliquam. Rem, commodi
-        inventore! Veniam sapiente accusamus nisi facilis expedita vel
-        provident. Vel, ullam aut!
-      </p>
-    </div>
-    <div class="sidebar">
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est magni
-        minima suscipit perferendis nam soluta, veniam voluptatibus! Eaque ipsum
-        ea, quam adipisci, assumenda dolor quod omnis quibusdam accusamus cumque
-        culpa modi voluptatem sunt ducimus consequuntur aliquam. Rem, commodi
-        inventore! Veniam sapiente accusamus nisi facilis expedita vel
-        provident. Vel, ullam aut!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est magni
-        minima suscipit perferendis nam soluta, veniam voluptatibus! Eaque ipsum
-        ea, quam adipisci, assumenda dolor quod omnis quibusdam accusamus cumque
-        culpa modi voluptatem sunt ducimus consequuntur aliquam. Rem, commodi
-        inventore! Veniam sapiente accusamus nisi facilis expedita vel
-        provident. Vel, ullam aut!
-      </p>
-    </div>
+    <CustomText>Home</CustomText>
+    <Avatar />
   </div>
 </template>
 
 <script>
-// import CustomText from '../../components/CustomText.vue'
+import CustomText from '../../components/CustomText.vue'
+import Avatar from '@/components/Avatar.vue'
 export default {
   name: 'Home',
-  components: {}
+  components: {
+    CustomText,
+    Avatar
+  }
 }
 </script>
 
-<style scoped>
-.home {
-  max-width: 600px;
-  margin-left: auto;
-  margin-right: auto;
-  @media (--t) {
-    max-width: none;
-    display: grid;
-    grid-template-columns: 1fr 293px;
-    grid-column-gap: 30px;
-  }
-}
-.timeline {
-  max-width: 615px;
-}
-.sidebar {
-  display: none;
-  @media (--t) {
-    display: block;
-  }
-}
-</style>
+<style scoped></style>
